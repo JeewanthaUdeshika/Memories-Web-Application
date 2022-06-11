ff8a0a28323ebbc91e8f4a76965688c908217791
JeewanthaUdeshika <e18028@eng.pdn.ac.lk>
2022-06-11 10:30:52 +0530

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+// connecting react application to the index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+// Rendering the app component
+ReactDOM.render(<App/>, document.getElementById('root'));
+
---
+++ server/index.js
@@ -0,0 +1,24 @@
+// we can use import keyword other than using require keyword by adding "type":"module" to package.json file
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+// Getting started with creating a object
+const app = express();
+
+// use() is used to mount middleware function (function that has access to req obj and res obj)
+app.use(bodyParser.json({limit: "30mb", extended: true}));      // Make limits to buffer data
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));      // Make limits to url encoded data
+app.use(cors());
+
+const CONNECT_URL = 'mongodb+srv://juaghost:juaghost@cluster0.qsvvems.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+// connect mongoDBB using mongoose
+mongoose.connect(CONNECT_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => app.listen(PORT, ()=>{console.log(`Server running on port: ${PORT}`)}))
+        .catch((error)=>{console.log(error.message)});
+
+mongoose.set('useFindAndModify', false);        // This prevents some warnings from the compiler
+
